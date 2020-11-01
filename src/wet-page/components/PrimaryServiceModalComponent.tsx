08659ee15885642dd7f516e241dbe0912b8f9e92
Marta Maier <martamaier@MacBook-Air-Marta.local>
2020-11-01 17:25:15 +0100

    added modal handling for primary services

--- src/wet-page/components/PrimaryServiceModalComponent.tsx
+++ src/wet-page/components/PrimaryServiceModalComponent.tsx
@@ -1,26 +1,37 @@
 import React from "react";
 import {PrimaryServiceCard} from "../../models/PrimaryServiceCard.model";
 import {CURRENT_ENV} from "../../environment";
+import styles from './PrimaryServiceModalComponent.module.scss';
+import { ReactComponent as Paw } from './../../images/paw.svg';
 
 function PrimaryServiceModalComponent({ title, image, description, details }: PrimaryServiceCard) {
     return (
-        <div>
-            {title}
-            <img src={`${CURRENT_ENV}/${image}`} alt={title}/>
-            {
-                description.length ? <p>{description}</p> : null
-            }
-            {
-                details.length ? (
-                    <ul>
-                        {
-                            details.map((detail: string) => (
-                                <li key={detail}>{detail}</li>
-                            ))
-                        }
-                    </ul>
-                ) : null
-            }
+        <div className={styles.primaryServiceContainer}>
+            <div className={styles.containerHeading}>
+                <img src={`${CURRENT_ENV}/${image}`} alt={title}/>
+                <h2>{title}</h2>
+            </div>
+            <div className={styles.containerBody}>
+                <div className={styles.containerContent}>
+                    {
+                        description.length ? <p>{description}</p> : null
+                    }
+                    {
+                        details.length ? (
+                            <ul>
+                                {
+                                    details.map((detail: string) => (
+                                        <li key={detail}>
+                                            <Paw className={styles.icon} />
+                                            {detail}
+                                        </li>
+                                    ))
+                                }
+                            </ul>
+                        ) : null
+                    }
+                </div>
+            </div>
         </div>
     )
 }
